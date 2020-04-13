d37a97488a25998c1aa2fcc9db1c1d9645c199a5
rndtim <51507837+rndtim@users.noreply.github.com>
2020-04-13 22:07:15 +0200

    CatSlider

--- client/components/slider/CategoriesSlider.vue
+++ client/components/slider/CategoriesSlider.vue
@@ -18,6 +18,11 @@
                   :key=index
                 >
                 {{ item.txt }}
+                <div v-if="ploader = true" class="ploader">
+                  <div class="dot" id="dot1"></div>
+                  <div class="dot" id="dot2"></div>
+                  <div class="dot" id="dot3"></div>
+                </div>
               </li>
             </ul>
           </div>
