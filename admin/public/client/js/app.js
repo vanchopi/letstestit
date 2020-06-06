46c1d443f533bd0552b8f11875adcd7720547f28
rndtim <51507837+rndtim@users.noreply.github.com>
2020-06-06 21:28:01 +0200

    unexpected vendor

---
+++ admin/public/client/js/app.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],{
+
+/***/ "./resources/client/assets/js/app.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'C:\\Dump\\OSPanel\\domains\\letstestit\\admin\\resources\\client\\assets\\js\\app.js'");
+
+/***/ }),
+
+/***/ 1:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./resources/client/assets/js/app.js");
+
+
+/***/ })
+
+},[1]);
