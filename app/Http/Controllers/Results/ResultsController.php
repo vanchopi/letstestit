614a0a50a9a999abbd8d51ba4068ba239cb876bd
vanchopi <ivan.ugnenko@webregul.ru>
2020-07-18 23:31:37 +0300

    results for test type 1

--- app/Http/Controllers/Results/ResultsController.php
+++ app/Http/Controllers/Results/ResultsController.php
@@ -10,25 +10,39 @@ use Illuminate\Http\Request;
 
 class ResultsController extends Controller
 {
-    public function getResult(Request $request){    	    	
-    	//$test = Test::findOrFail($id);  
-    	$tests = 'Здорова!' . $request->id;   	
-    	$img = self::getMedias($request->id);
+    public function getResult(Request $request){    	    	    	   	
+    	$test = Test::findOrFail($request->id);
+    	$type = $test->type;
+    	//print_r($type);
     	
-    	/*for ($i=0; $i < count($tests); $i++) { 
-    		
-            $media = self::getMedias($tests[$i]->id);
-            $tests[$i]->bg_image = $media->bg_image;
-            $tests[$i]->main_image = $media->main_image;                        
-    	}    	
-        return $tests;*/
-        $results[] = (object) [
-	        "img" => $img[0]->result_image,
-	        "result" => 'Ты - Добби!',
-	        "description" => 'Судьба была неблагосклонна: твое предназначение — быть на посылках у настоящих волшебников. Но смелость, благородство и вера в добро помогли тебе добиться самого желанного — свободы. Наглядный пример того, что даже один в поле может быть еще каким воином.',
+    	$result = Result::select('variants')->where('test_id',$request->id)->first(); 
+    	$result = json_decode($result->variants);
+    	$userAnswers = $request->answers;
+    	$questions = json_decode($test->questions);
+    	//$testAnswers = 
+    	$img = self::getMedias($request->id);
+    	//echo "********";
+    	//print_r(json_decode($result->variants));
+    	switch ($type) {
+		    case 'knowledges':
+		        $num = self::checkKnowledges($result, $userAnswers, $questions);
+		        break;
+		    case 'tree':
+		        echo $type;
+		        break;		    
+		    default:
+		       echo "хрен знает чё";
+		};
+		//echo "********";
+        $results = [
+        	"success" => true,
+	        "img" => $img[$num]->result_image,
+	        "result" => $result[$num]->result,
+	        "description" => $result[$num]->description,
 	        "id" => $request->id
 	    ];
-        return response()->json($results[0]);
+	    //print_r($results);
+        return response()->json($results);        
     }
 
     static public function getMedias($id){
@@ -38,9 +52,72 @@ class ResultsController extends Controller
    			$arr[$i] = (object) [
 	            "result_image" => $media[$i]->id . '/' . $media[$i]->file_name,	            
 	        ];
-   		}   		
-        //print_r($arr);
-        //print_r($item);
+   		}   		        
+    	return $arr;
+    }
+
+    static public function checkKnowledges($result, $userAnswers, $testAnswers){
+    	//echo "1. ********";
+    	//print_r($result);
+    	/*echo "2. ********";
+    	print_r($userAnswers);
+    	echo "3. ********";
+    	print_r($testAnswers);*/
+    	$error = false;
+    	$correct = 0;
+    	for ($i=0; $i < sizeof($userAnswers); $i++) { 
+    		$id = $userAnswers[$i]['id'];    		
+    		if ($testAnswers[$i]->answers[$id]->checked > 0 ){
+    			$correct++;
+    		}
+    	}
+    	//echo "result size - " . sizeof($result);
+    	$num = sizeof($result);
+    	$sortedResult = self::subSort($result, 'value');
+    	//echo "--------";
+    	//print_r($sortedResult);
+    	//echo "--------";
+    	$numResult = 0;
+    	if ($correct > 0){
+	    	if ($num > 1){
+	    		if ($correct >= $sortedResult[$num - 1]->value){
+	    			$numResult = $sortedResult[$num - 1]->id;
+	    			//echo "1.*";
+	    		}else{
+	    			for ($i=0; $i < $num - 1; $i++) { 
+						if ( ($sortedResult[$i]->value <= $correct) && ($correct <= $sortedResult[$i + 1]->value) ){
+							$numResult = $sortedResult[$i]->id;
+							//echo "2.*";
+						}
+	    			}
+	    		}
+		    	
+		    } elseif ($num == 1){
+		    	$numResult = 0;
+		    	//echo "3.*";
+		    } else{
+		    	//echo " empty ";
+		    	$error = true;
+		    	//echo "4.*";
+		    }
+		}else{
+			$numResult = 0;
+			//echo "5.*";
+		}
+    	//$ansArray = 
+    	//echo "correct answers - " . $correct;
+    	return $numResult;
+    }
+
+    static public function subSort($arr, $keyVal){
+    	usort($arr, function($a, $b){
+		    if( $a->value == $b->value )
+		        return 0;
+		    else if($a->value > $b->value)
+		        return 1;
+		    else
+		        return -1;
+		});
     	return $arr;
     }
 }
