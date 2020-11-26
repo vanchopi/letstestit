e600231580089851debb51c37ca20cadb69a2d39
vanchopi <ivan.ugnenko@webregul.ru>
2020-11-26 20:56:43 +0300

    current changes in admin

--- admin/resources/client/assets/js/components/cruds/Tests/createImage/CreateImage.vue
+++ admin/resources/client/assets/js/components/cruds/Tests/createImage/CreateImage.vue
@@ -51,8 +51,8 @@
             <div class="img-wrp">                                                            
                 <img src="" alt="" id="thumb">
             </div>              
-            <canvas width="512" height="228" id="canvas1" style='position: relative; left:0px; top:0px; display: none;'></canvas>
-            <canvas width="512" height="228" id="canvas2" style='position: relative; left:0px; top:0px; display: none;'></canvas>
+            <canvas :width="wd" :height="ht" id="canvas1" style='position: relative; left:0px; top:0px; display: none;'></canvas>
+            <canvas :width="wd" :height="ht" id="canvas2" style='position: relative; left:0px; top:0px; display: none;'></canvas>
             <img id="resultImg" src="" alt="">
             <div class="btn-wrp">
               <a class="btn btn-primary btn-sm" href="#" @click.prevent="generateImg">Generate image</a>
@@ -79,7 +79,9 @@ export default {
               title: '',
               result: '',
               description: '',
-            }
+            },
+            wd: 1024,
+            ht: 456,
         }
     },
     watch:{
@@ -135,29 +137,31 @@ export default {
                 canvas2 = document.getElementById("canvas2"),                
                 img = document.getElementById("thumb"),
                 resultImg = document.getElementById("resultImg"),
+                wd = this.wd,
+                ht = this.ht,
                 options = {
                     offsetX: 0,
                     offsetY: 0,
-                    width: 512,
-                    height: 228,
+                    width: this.wd,
+                    height: this.ht,
                 };
           //console.log('custom img - ', img);
             rasterizeHTML.drawHTML(
-                '<div style="width: 512px; height: 228px; padding: 15px; background-image: url('+ img.src +'); background-size: cover;">' +
-                '<div class="title-wrp" style="display: flex; align-items: center; justify-content: flex-start; padding-bottom: 15px; margin-bottom: 15px; width: calc(100% - 40px); border-bottom: 1px solid white;">'+ 
-                '<img width="70" src="/logo.png" style="margin-right: 0px;">' + 
-                '<b style="position: absolute; margin-left: 80px;">' +
+                '<div style="width:'+ wd + 'px; height:'+ ht + 'px; padding: 30px; background-image: url('+ img.src +'); background-size: cover;">' +
+                '<div class="title-wrp" style="display: flex; align-items: center; justify-content: flex-start; padding-bottom: 30px; margin-bottom: 30px; width: calc(100% - 70px); border-bottom: 1px solid white;">'+ 
+                '<img width="140" src="/logo.png" style="margin-right: 0px;">' + 
+                '<b style="position: absolute; margin-left: 150px; font-size: 28px;">' +
                 '<span style="color: white;">'+ this.imgText.title +'</span>'+
                 '</b>'+
                 '</div>'+
-                '<span style="color: white; font-size: 22px; font-weight: bold; margin-bottom: 5px; display: block; font-family: Open Sans,sans-serif;">Мой результат : ' + this.imgText.result + '</span>'+
-                '<span style="color: white; font-size: 12px; font-weight: normal; margin-bottom: 15px; display: block; font-family: Open Sans,sans-serif;">' + this.imgText.description + '</span>'+
+                '<span style="color: white; font-size: 34px; font-weight: bold; margin-bottom: 15px; display: block; font-family: Open Sans,sans-serif;">Мой результат : ' + this.imgText.result + '</span>'+
+                '<span style="color: white; font-size: 28px; font-weight: normal; margin-bottom: 25px; display: block; font-family: Open Sans,sans-serif;">' + this.imgText.description + '</span>'+
                 '</div>', 
                 canvas).then(function(renderResult){
                 let contextCanvas2 = canvas2.getContext('2d');                                   
                 var img2 = new Image;
                 img2.onload = function() {
-                    contextCanvas2.drawImage(img2, -10, -10, 522, 238); // width > 1000px
+                    contextCanvas2.drawImage(img2, -10, -10, wd + 10, ht + 10); // width > 1000px
                     resultImg.src = canvas2.toDataURL('image/png');
                 };
                 img2.src = canvas.toDataURL('image/png');
