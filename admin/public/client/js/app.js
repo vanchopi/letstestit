db3f833e8564a512645e41a6cebb5ae1502c5e1e
vanchopi <ivan.ugnenko@webregul.ru>
2020-12-17 01:05:23 +0300

    seo tests mask

--- admin/public/client/js/app.js
+++ admin/public/client/js/app.js
@@ -2372,6 +2372,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
     computed: _extends({}, Object(__WEBPACK_IMPORTED_MODULE_0_vuex__["mapGetters"])('TestsSingle', ['item', 'resultsItem', 'loading', 'categoriesAll'])),
     created: function created() {
         this.fetchCategoriesAll();
+        this.setSeoMask();
         this.seoOnInput();
     },
     destroyed: function destroyed() {
@@ -2385,6 +2386,8 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
         updateTitle: function updateTitle(e) {
             this.setTitle(e.target.value);
             this.txtTitle = e.target.value;
+            this.setSeoMask();
+            this.seoOnInput();
         },
         checkNum: function checkNum(val) {
             if (val < 0) {
@@ -2636,6 +2639,15 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
         },
         seoOnInput: function seoOnInput() {
             this.setSeo(this.seo);
+        },
+        setSeoMask: function setSeoMask() {
+            var title = this.item.title == null ? '' : this.item.title;
+            return this.seo = {
+                title: '' + title,
+                h1: '\u041F\u0440\u043E\u0439\u0434\u0438 \u0442\u0435\u0441\u0442 ' + title + ' \u043D\u0430 \u0441\u0430\u0439\u0442\u0435 letstestit.ru',
+                description: '\u0425\u043E\u0447\u0435\u0448\u044C \u0443\u0437\u043D\u0430\u0442\u044C \u043E \u0441\u0435\u0431\u0435 \u043C\u0430\u043A\u0441\u0438\u043C\u0443\u043C. \u0422\u0435\u0441\u0442 ' + title + ' \u043F\u043E\u043C\u043E\u0436\u0435\u0442 \u0442\u0435\u0431\u0435 \u0432 \u044D\u0442\u043E\u043C. \u0421\u043A\u043E\u043D\u0446\u0435\u043D\u0442\u0440\u0438\u0440\u0443\u0439\u0441\u044F, \u0438 \u043F\u043E\u0441\u0442\u0430\u0440\u0430\u0439\u0441\u044F \u043E\u0442\u0432\u0435\u0442\u0438\u0442\u044C \u043D\u0430 \u0432\u0441\u0435 \u0432\u043E\u043F\u0440\u043E\u0441\u044B \u0447\u0435\u0441\u0442\u043D\u043E \u043D\u0430 \u0441\u0430\u0439\u0442\u0435 letstestit.ru',
+                keywords: '\u0442\u0435\u0441\u0442 ' + title + ', \u043E\u043D\u043B\u0430\u0439\u043D \u0442\u0435\u0441\u0442, \u043F\u0440\u043E\u0439\u0442\u0438 \u0442\u0435\u0441\u0442'
+            };
         },
         submitForm: function submitForm() {
             var _this3 = this;
@@ -37816,7 +37828,7 @@ var render = function() {
                                 id: "seo-decsription",
                                 placeholder: "",
                                 cols: "60",
-                                rows: "1",
+                                rows: "2",
                                 required: ""
                               },
                               domProps: { value: _vm.seo.description },
