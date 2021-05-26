5139eaa99b77b09cb5d83688885658cf91abd842
vanchopi <ivan.ugnenko@webregul.ru>
2021-05-27 00:04:48 +0300

    current changes

--- client/components/about/About.vue
+++ client/components/about/About.vue
@@ -68,7 +68,7 @@
 import { mapGetters, mapState } from 'vuex'
 import { getCategoriesList } from '~/api/categories/category'
 import TextLoader from '~/components/global/TextLoader'
-import FeedbackForm from '~/components/FeedbackForm/FeedbackForm'
+import FeedbackForm from '~/components/feedbackForm/FeedbackForm'
 
 export default {
   name: 'About',
