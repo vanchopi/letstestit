d5807fe367847d0bbc47296a1c7ab559975d32a3
Ivan Ugnenko <vanchopi@yahoo.com>
2023-03-26 00:32:32 +0400

    current changes

--- admin/public/client/js/app.js
+++ admin/public/client/js/app.js
@@ -5253,22 +5253,41 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var vuex__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vuex */ "./node_modules/vuex/dist/vuex.esm.js");
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "Main",
   components: {},
   props: {},
-  computed: {},
+  computed: {
+    affilates: function affilates() {
+      return this.$store.state.Affilates.affilates;
+    }
+  },
   data: function data() {
     return {
       result: null,
-      showModal: false
+      showModal: false,
+      banner: null
     };
   },
-  created: function created() {},
-  methods: {
-    // ...mapActions('Affilates', ['get']),    
-  }
+  created: function created() {
+    this.getAffilates();
+  },
+  methods: _objectSpread(_objectSpread({}, (0,vuex__WEBPACK_IMPORTED_MODULE_0__.mapActions)('Affilates', ['get'])), {}, {
+    getAffilates: function getAffilates() {
+      this.get();
+    },
+    submitForm: function submitForm() {
+      console.log('submitForm');
+    }
+  })
 });
 
 /***/ }),
@@ -8858,7 +8877,35 @@ __webpack_require__.r(__webpack_exports__);
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
-  return _c('div', [_vm._v("\n  Main\n")]);
+  return _c('form', {
+    on: {
+      "submit": function submit($event) {
+        $event.preventDefault();
+        return _vm.submitForm.apply(null, arguments);
+      }
+    }
+  }, [_c('div', {
+    staticClass: "form-group"
+  }, [_c('label', {
+    attrs: {
+      "for": "banner"
+    }
+  }, [_vm._v("Баннер")]), _vm._v(" "), _c('select', {
+    staticClass: "form-control",
+    attrs: {
+      "id": "banner"
+    }
+  }, [_c('option', {
+    attrs: {
+      "selected": ""
+    }
+  }, [_vm._v("Выберите...")]), _vm._v(" "), _vm._l(_vm.affilates, function (affilate) {
+    return _c('option', {
+      domProps: {
+        "value": affilate.id
+      }
+    }, [_vm._v("\n        " + _vm._s(affilate.description) + "\n      ")]);
+  })], 2)])]);
 };
 var staticRenderFns = [];
 render._withStripped = true;
--- admin/resources/client/assets/js/components/cruds/Main/Tabs/Main.vue
+++ admin/resources/client/assets/js/components/cruds/Main/Tabs/Main.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    Main
-  </div>
+  <form @submit.prevent="submitForm">
+    <div class="form-group">
+      <label for="banner">Баннер</label>
+      <select class="form-control" id="banner">
+        <option selected>Выберите...</option>
+        <option 
+          v-for="affilate in affilates"
+          :value="affilate.id"
+          >
+          {{affilate.description}}
+        </option>
+      </select>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -13,18 +24,29 @@ export default {
   },
   props: {    
   },
-  computed: {    
+  computed: {   
+    affilates(){
+      return this.$store.state.Affilates.affilates;
+    } 
   },
   data(){
     return {
       result: null,
-      showModal: false,      
+      showModal: false,   
+      banner: null,   
     }
   },
   created(){
+    this.getAffilates();
   },
   methods: {
-    // ...mapActions('Affilates', ['get']),    
+    ...mapActions('Affilates', ['get']),
+    getAffilates(){
+      this.get();
+    },  
+    submitForm(){
+      console.log('submitForm');
+    }
   }
 };
 </script>
